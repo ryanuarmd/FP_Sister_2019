33e11aafa168e3f33302bfe3a1e7cd2d1a2f8e52
Akmal Darari <akmal.darari16@mhs.if.its.ac.id>
2019-12-03 18:43:43 +0700

    Penyempurnaan

--- Game dasar/tictactoe_dasar.py
+++ Game dasar/tictactoe_dasar.py
@@ -29,17 +29,17 @@ def display_board(board):
 def player_input():
     marker = ' '
     while not (marker == 'X' or marker == 'O'):
-        marker = raw_input('Player 1, Choose O or X to play!').upper()
+        marker = raw_input('Pemain 1, Pilih O atau X untuk bermain!').upper()
     if marker == 'X':
-        return {'Player 1': 'X', 'Player 2': 'O'}
+        return {'Pemain 1': 'X', 'Pemain 2': 'O'}
     else:
-        return {'Player 2': 'X', 'Player 1': 'O'}
+        return {'Pemain 2': 'X', 'Pemain 1': 'O'}
 
 def win_check (board):
     return any(board[a] != ' ' and board[a] == board[b] == board[c] for a, b, c in WIN_COMBINATIONS)
 
 def choose_first(players):
-    random_player = 'Player {}'.format(random.randint(1, 2))
+    random_player = 'Pemain {}'.format(random.randint(1, 2))
     return random_player, players[random_player]
 
 def full_check (board):
@@ -48,38 +48,46 @@ def full_check (board):
 def player_choice(board):
     while True:
         try:
-            position = int(raw_input('Choose number input 1-9'))
+            position = int(raw_input('Silahkan masukkan angka 1-9'))
             if position in range(1, 9) and board[position] == ' ':
                 return position
         except ValueError:
             pass
 
 def replay():
-    return raw_input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
+    return raw_input('Maukah anda bermain lagi? Ketik Ya atau Tidak: ').lower().startswith('y')
 
 def ttt():
     board = [' ' for _ in range(10)]
     players = player_input()
     name, player_marker = choose_first(players)
-    print('{} with marker {} will go first.'.format(name, player_marker))
+    print('{} dengan tanda {} akan bermain pertama.'.format(name, player_marker))
     while True:
         position = player_choice(board)
         board[position] = player_marker
         display_board(board)
         if win_check(board):
-            print('Congratulations {}! You have won the game!'.format(name))
+            print('Selamat {}! Anda telah memenangkan permainan ini!'.format(name))
             break
 
         if full_check(board):
-            print('Congratulations {} and {}! You have a tie!'.format(players.keys()))
+            print('Selamat {} dan {}! Anda telah menang seri!'.format(players.keys()))
             break
 
-        name = 'Player 1' if name == 'Player 2' else 'Player 2'
+        name = 'Pemain 1' if name == 'Pemain 2' else 'Pemain 2'
         player_marker = players[name]
         print(name, player_marker)
 
 if __name__ == '__main__':
-    print('Welcome to Tic Tac Toe Game!')
+    print('Selamat Datang di permainan Tic Tac Toe!')
+    print('Aturan permainan: ')
+    print('Isi angka 1-9 di kolom input. Angka tersebut akan memposisikan dimana posisi simbol X atau O yang anda inginkan')
+    print('Jika sudah terisi ketiganya secara horizontal/vertikal/diagonal, anda menang\n')
+    print('Untuk format barisan dan kolom adalah seperti ini:')
+    print('(1 2 3)')
+    print('(4 5 6)')
+    print('(7 8 9)\n')
+    print('Simpel kan? Selamat mencobaaaa')
     while True:
         ttt()
         if not replay():
